633ee59c9a3a867c275aacc9d087a03ad1e3db72
Giovazz89 <giovazz89@gmail.com>
2026-05-09 15:50:56 +0200

    First draft
    First plugin draft, to be tested and tweaked with

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,148 +1,75 @@
-import type { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import * as ModbusRTUImport from 'modbus-serial';
+import { SajH2ModbusBatteryPlatform } from './platform.js';
 
-import type { ExampleHomebridgePlatform } from './platform.js';
+// Trick to prevent constructor error in ESM
+const ModbusRTU = (ModbusRTUImport.default || ModbusRTUImport) as any;
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
+export class SajBatteryAccessory {
   private service: Service;
-
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private client = new ModbusRTU();
+  
+  // internal state data
+  private state = {
+    soc: 0,
+    chargingState: 0, // 0 = Not Charging, 1 = Charging
+    statusLowBattery: 0,
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: SajH2ModbusBatteryPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
-    // set accessory information
+    // device info
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'SAJ')
+      .setCharacteristic(this.platform.Characteristic.Model, 'H2-6K-S');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
+    // create Battery Service
+    this.service = this.accessory.getService(this.platform.Service.Battery) || 
+                   this.accessory.addService(this.platform.Service.Battery);
 
-    if (accessory.context.device.CustomService) {
-      // This is only required when using Custom Services and Characteristics not support by HomeKit
-      this.service = this.accessory.getService(this.platform.CustomServices[accessory.context.device.CustomService]) ||
-        this.accessory.addService(this.platform.CustomServices[accessory.context.device.CustomService]);
-    } else {
-      this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    // start update loop
+    this.updateData();
+    setInterval(() => this.updateData(), (this.platform.config.interval || 30) * 1000);
+  }
+
+  async updateData() {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connectTCP(this.platform.config.ip, { port: this.platform.config.port });
+        this.client.setID(this.platform.config.slaveId);
+      }
+
+      // read the 4 registers (base 40960 + offset 12)
+      const startRegister = 40960 + 12;
+      const response = await this.client.readHoldingRegisters(startRegister, 4);
+      
+      const rawSOC = response.data[0];
+      const soc = rawSOC * 0.01;
+      
+      // read battery current (offset 15, so index 3 in 4 registers buffer)
+      // use readInt16BE to manage sign correctly
+      const current = response.buffer.readInt16BE(6) * 0.01;
+
+      // inverted logic (empirically confirmed):
+      // negative = charging, positive = discharging
+      const isCharging = current < -0.1;
+      
+      this.state.soc = soc;
+      this.state.chargingState = isCharging ? 1 : 0;
+      this.state.statusLowBattery = soc < 15 ? 1 : 0;
+
+      // update HomeKit
+      this.service.updateCharacteristic(this.platform.Characteristic.BatteryLevel, this.state.soc);
+      this.service.updateCharacteristic(this.platform.Characteristic.ChargingState, this.state.chargingState);
+      this.service.updateCharacteristic(this.platform.Characteristic.StatusLowBattery, this.state.statusLowBattery);
+
+      this.platform.log.debug(`Updated: SOC ${soc}%, Charging: ${isCharging}`);
+
+    } catch (e) {
+      this.platform.log.error('Error during Modbus reading:', e);
+      this.client.close(() => {});
     }
-
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
-
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this)); // SET - bind to the `setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same subtype id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name')
-      || this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name')
-      || this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-  }
-
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possible. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-   * In this case, you may decide not to implement `onGet` handlers, which may speed up
-   * the responsiveness of your device in the Home app.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
   }
 }
